69b7c4c8180b29cd229caf9006930609cfd465ad
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-12 16:54:37 +0100

    Add EasyAdmin integration for CannabisProduct management
    Introduced EasyAdmin support with a `DashboardController` and a CRUD controller for managing `CannabisProduct` entities. Updated `.gitignore` to include the new public assets directory. Also added licensing information for Bootstrap within the project.

---
+++ src/Controller/Admin/CannabisProductCrudController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\CannabisProduct;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints\Image;
+
+class CannabisProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return CannabisProduct::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+                ->setRequired(false)
+                ->hideOnForm(),
+            TextField::new('name'),
+            TextField::new('type'),
+            TextField::new('thcContent'),
+            TextField::new('cbdContent'),
+            ImageField::new('imageUrl')
+                ->setBasePath('/uploads/images')
+                ->setUploadDir('assets/images')
+                ->setFileConstraints(new Image(maxSize: '900k'))
+                ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]')
+                ->setRequired(false)
+                ->setLabel('Image (Size 268x175 Max. 900kb)'),
+        ];
+    }
+
+}
