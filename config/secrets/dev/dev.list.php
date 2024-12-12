599b00bfba5e73dde07c1fd8efde94f4dfdf4fbc
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-12 13:04:51 +0100

    Add encrypted MySQL secrets for dev and update prod secrets
    Introduced encrypted MySQL database credentials for the development environment, including database, host, user, password, and

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,9 +1,14 @@
 <?php
 
 return [
+    'MYSQL_DATABASE' => null,
+    'MYSQL_HOST' => null,
+    'MYSQL_PASSWORD' => null,
+    'MYSQL_PORT' => null,
     'MYSQL_TEST_DATABASE' => null,
     'MYSQL_TEST_HOST' => null,
     'MYSQL_TEST_PASSWORD' => null,
     'MYSQL_TEST_PORT' => null,
     'MYSQL_TEST_USER' => null,
+    'MYSQL_USER' => null,
 ];
