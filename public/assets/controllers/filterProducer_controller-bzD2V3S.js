f20a34eeebb1c2ff533cf8474534043eec5ea647
Jan-Peter Wittig <jan-peter.wittig@live.com>
2024-12-16 14:09:17 +0100

    Add new EasyAdmin font assets and related JS files
    This commit introduces new font files for EasyAdmin, including regular, solid, brand, and v4 compatibility variants in TTF and WOFF2 formats. Additionally, it includes a related JavaScript file and its corresponding license file for Bootstrap v5.3.3. These updates ensure proper asset availability and licensing compliance.

---
+++ public/assets/controllers/filterProducer_controller-bzD2V3S.js
@@ -0,0 +1,33 @@
+import { Controller } from '@hotwired/stimulus';
+
+let filterCheckboxes;
+
+export default class extends Controller {
+    connect() {
+        console.log('filterProduct_controller connected');
+        filterCheckboxes = document.querySelectorAll('.filter-checkbox');
+    }
+
+    reloadProducts(event) {
+        const frame = document.getElementById('products-frame');
+        const url = frame.getAttribute("datasrc");
+
+        let filterValues = [];
+
+        for (let i = 0; i < filterCheckboxes.length; i++) {
+            if (!filterCheckboxes[i].checked) {
+                continue;
+            }
+            if (filterCheckboxes[i].classList.contains('filter-checkbox-producer')) {
+                filterValues.push(filterCheckboxes[i].getAttribute('id'));
+            }
+        }
+
+        frame.src = url + '?filterProducer=' + JSON.stringify(filterValues);
+    }
+
+    toggleFilterVisibility(event) {
+        const filter = document.getElementById('filter-producer-frame');
+        filter.classList.toggle('d-none');
+    }
+}
